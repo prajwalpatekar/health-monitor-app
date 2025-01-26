cfe9a900e28186731e61e1705a73a17c91cc6ad0
Prajwal Namdev Patekar <prajwal@Prajwals-MacBook-Air.local>
2025-01-26 17:18:44 +0530

    new changes for conflicts test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-500">
-        Welcome to Next.js with Tailwind CSS!
+        Welcome to Next.js!!!!
       </h1>
     </div>
   );
